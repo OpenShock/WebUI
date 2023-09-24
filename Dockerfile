b123a2c72a349e93d15bc67d3270467e69ac6c74
Red Mushie <github@redmushie.me>
2023-09-24 10:13:06 +0200

    build: Add init script to substitute env variables at runtime

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,15 @@
 FROM nginx:1-alpine
+
+ENV SHOCKLINK_API_DOMAIN=api.shocklink.net
+
+# Copy release artifacts (static JS and CSS bundles)
 COPY dist /usr/share/nginx/html
+
+# Copy custom startup script.
+# This script performs environment variable substitution!
+COPY startup.sh .
+
+# Start up nginx using the alternative entrypoint, but with the default CMD.
+# See: https://github.com/nginxinc/docker-nginx/blob/master/mainline/debian/Dockerfile#L113-L119
+ENTRYPOINT ["/startup.sh"]
+CMD ["nginx", "-g", "daemon off;"]
---
+++ startup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Variables for startup script
+SERVE_PATH=/usr/share/nginx/html
+NAME=ShockLink
+
+require() {
+    if [ -z "$1" ]; then
+        echo "Env var '$1' is required. Specify it and restart the container."
+        exit 1
+    fi
+}
+
+# Method that accepts one argument, the name of the environment variable to inject.
+# Will substitute said variable as a literal string 
+inject() {
+    if [ -z "$1" ] && [ -z "$2" ]; then
+        echo "[$NAME] Usage: inject <variable> <value>"
+    fi
+
+    # Ensure the environment variable has a value specified.
+    require $1
+
+    echo "[$NAME] Injecting variable: $1 = $2"
+    sed -i 's/$1/${!var}/g' $SERVE_PATH/*.js
+}
+
+# Inject our variables.
+inject SHOCKLINK_API_DOMAIN $SHOCKLINK_API_DOMAIN
+
+# Start nginx as normal.
+# If something breaks, see: https://github.com/nginxinc/docker-nginx/blob/master/mainline/debian/Dockerfile
+echo "[$NAME] Starting nginx"
+./docker-entrypoint.sh "$@"
