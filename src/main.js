1fffac05ad7c8f3d9b4ab94d1c3fd5a79b111455
LucHeart <luca@perugia.ovh>
2023-09-30 19:12:04 +0200

    Dev sentry

--- src/main.js
+++ src/main.js
@@ -58,6 +58,25 @@ app.config.globalProperties.emitter = emitter;
 app.config.devtools = true;
 global.emitter = emitter;
 
+Sentry.init({
+	app,
+	dsn: "https://052d20b68586c85c90b4e454bbf85a69@o4505946910949376.ingest.sentry.io/4505946978910208",
+	integrations: [
+	  new Sentry.BrowserTracing({
+		// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+		tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+		routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+	  }),
+	  new Sentry.Replay(),
+	],
+	// Performance Monitoring
+	tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+
+
 app.mount('#app');
 
 const footerApp = createApp(Footer)
